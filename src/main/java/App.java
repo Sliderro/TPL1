5c9ff0bacf4927818cdef2075d866d9494c18570
Sliderro <arek.debski2@gmail.com>
2018-10-23 14:21:19 +0200

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,20 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        GridPane root=new GridPane();
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
