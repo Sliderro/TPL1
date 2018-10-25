fae8cb7d3074d85141437ceafe0da82a001cb0e2
Sliderro <44056481+Sliderro@users.noreply.github.com>
2018-10-25 12:11:35 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,7 +10,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-//xd
+//lol
 
 public class App extends Application {
 
